6b6a04021b50f07b36e8ddd04eb4d99f1a74c81a
fzheng <feng.zheng@freecause.com>
2014-03-31 18:31:56 -0400

    edit test case

---
+++ tests/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = [
+  {
+    url: "http://www.macys.com"
+  },
+  {
+    url: "http://www.bestbuy.com"
+  },
+  {
+    url: "http://www.target.com"
+  }
+];
--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,7 @@
 var CDriver = require('../');
 var cDriver = new CDriver();
 var logger = require('../log/');
-
-var req = [{
-  url: "http://www.macys.com"
-}, {
-  url: "http://www.bestbuy.com"
-}, {
-  url: "http://www.target.com"
-}];
+var req = require('./config.js');
 
 cDriver.run(req, function(err, data){
   if(err) logger.error(err); else logger.info(data);
